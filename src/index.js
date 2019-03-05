b203b79d360838d435de4d225a20d03afd7db473
fernandovallejo09 <www.fertron_14@hotmail.com>
2019-03-04 21:46:01 -0500

    Iniciando proyecto

---
+++ src/index.js
@@ -0,0 +1,39 @@
+//Framework para crear un srvidor facilmente
+const express = require('express')
+
+//Plugin para leer archivos
+
+
+const fs = require('fs')
+
+//Inciamos el servidor web desde el método express()
+const app = express()
+
+//Middlewares
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    let productos = JSON.parse(fs.readFileSync('src/db/productos.json', 'utf8'))
+    res.json(productos)
+})
+
+app.post('/', (req, res) => {
+    let productos = JSON.parse(fs.readFileSync('src/db/productos.json', 'utf8'))
+    productos.push(req.body)
+    
+    res.json(productos)
+})
+
+app.delete('/', (req, res) => {
+    res.send('Respuesta desde DELETE')
+})
+
+app.put('/', (erq, res) => {
+    res.send('Respuesta desde PUT')
+})
+//Método que permite al srvidor web escuchar en un puerto específico
+app.listen(3000), () => {
+    console.log('El servidor est acorreiendo en http://localhost:3000')
+}
+
